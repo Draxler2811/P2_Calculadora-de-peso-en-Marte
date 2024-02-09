4874b3b5fba8cd8214c0001ead9f1b1ba198888b
Draxler2811 <ol1964759@gmail.com>
2024-02-09 10:41:48 -0600

    Aver si ya

--- TecNM.Practica2.Core/Services/BmiService.cs
+++ TecNM.Practica2.Core/Services/BmiService.cs
@@ -7,12 +7,12 @@ namespace TecNM.Practica2.Core.Services;
 public class BmiService : IBmiService{
     public Bmi ProcessBmi(Person person){
 
-    var bmi = new Bmi();
+   var bmi = new Bmi();
    float gravedadTierra = 9.81F;
    float gravedadMarte = 3.711F;
 
     bmi.Index = person.Weight * (gravedadMarte / gravedadTierra);
-    
+
     return bmi;
     }
 }
